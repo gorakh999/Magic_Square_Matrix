7fa9edf2525f17d07e911ea143e05eafc6bce9bf
GORAKH GUPTA <gorakhgupta999@gmail.com>
2020-02-23 18:10:12 +0530

    Magic_Square_Matrix

---
+++ Magic_Square_Matrix.py
@@ -0,0 +1,58 @@
+'''A python program to find the magic square matrix '''
+''' a Square matrix is said to be a magic square matrix
+ if the sum of each rows / each column and diagonal is a constant 
+ number '''
+
+
+def magic_square(n):
+
+  magicSquare = []
+  for i in range(n):
+    l = []
+    for j in range(n):
+      l.append(0)
+    magicSquare.append(l)
+    
+  i = n//2
+  j = n-1
+
+  num = n*n
+  count = 1 
+
+  while (count<=num):
+
+    if (i == -1 and j==n):   #Condition 4
+        j =n-2
+        i =0
+    else:
+      if(j==n):    #coloumn value is exceeding
+          j=0
+
+      if(i<0):    # row is becoming -1
+          i = n-1
+
+    if(magicSquare[i][j]!=0):
+      j = j-2
+      i = i+1
+      continue
+
+      
+    else:
+      magicSquare[i][j] = count
+      count = count+1
+
+    i = i-1
+    j = j+1    #condition 1
+
+ 
+  for i in range(n):
+    
+    for j in range(n):
+      
+      print(magicSquare[i][j], end = " ")
+    print()
+
+  print("The sum of each row/coloumn/diagonal is :",(n*(n**2+1))/2)
+    
+n=int(input("Enter the size of Square Matrix you want to get Magic Square \n"))
+magic_square(n)
